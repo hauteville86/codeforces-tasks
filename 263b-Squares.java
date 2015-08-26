cec15c2b7cae44fe9ea8bf8ab3db36449bd2ca56
hauteville86 <karolcyprowski@gmail.com>
2015-08-26 10:46:49 +0200

    Create 263b-Squares.java

---
+++ 263b-Squares.java
@@ -0,0 +1,28 @@
+//http://codeforces.com/problemset/problem/263/B
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Squares {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int k = scanner.nextInt();
+		if (n >= k)
+		{
+			int[] coordinates = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				coordinates[i] = scanner.nextInt();
+			}
+			Arrays.sort(coordinates);
+			int x = 0;
+			int y = coordinates[n-k];
+			System.out.println(x + " " + y); 
+		}
+		else
+		{
+			System.out.println("-1");
+		}
+	}
+}
