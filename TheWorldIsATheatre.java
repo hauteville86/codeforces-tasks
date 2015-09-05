6ea0b42f6c1807fa6c432f28217090ad3b5503b2
Karol Cyprowski <karolcyprowski@gmail.com>
2015-09-05 11:46:31 +0200

    Create TheWorldIsATheatre.java

---
+++ TheWorldIsATheatre.java
@@ -0,0 +1,69 @@
+// http://codeforces.com/problemset/problem/131/C
+import java.util.Scanner;
+
+public class TheWorldIsATheatre
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		long BOYS_MIN = 4;
+		long GIRLS_MIN = 1;
+		long boys = scan.nextLong();
+		long girls = scan.nextLong();
+		long actors = scan.nextLong();
+		scan.close();
+		long combinations = 0;
+		for(long numOfBoysNeeded = BOYS_MIN; numOfBoysNeeded <= boys && numOfBoysNeeded < actors; numOfBoysNeeded++)
+		{
+			long numOfGirlsNeeded = actors - numOfBoysNeeded;
+			if(numOfGirlsNeeded <= girls)
+			{
+				long boysWithoutJob = boys - numOfBoysNeeded;
+				long powFactorBoys;
+				if(boysWithoutJob < numOfBoysNeeded)
+				{
+					powFactorBoys = boysWithoutJob;
+				}
+				else
+				{
+					powFactorBoys = numOfBoysNeeded;
+				}
+
+				long girlsWithoutJob = girls - numOfGirlsNeeded;
+				long powFactorGirls;
+				if(girlsWithoutJob < numOfGirlsNeeded)
+				{
+					powFactorGirls = girlsWithoutJob;
+				}
+				else
+				{
+					powFactorGirls = numOfGirlsNeeded;
+				}
+				powFactorGirls = girls - numOfGirlsNeeded;
+
+
+				long boysCombinations = 1;
+				long divider = 1;
+				for(long i = 1; i <= powFactorBoys; i++)
+				{
+					boysCombinations = boysCombinations * (boys + 1 - i);
+					divider = divider * i;
+				}
+				boysCombinations = boysCombinations / divider;
+				System.out.println("boysCombinations = " + boysCombinations);
+
+				long girlsCombinations = 1;
+				divider = 1;
+				for(long i = 1; i <= powFactorGirls; i++)
+				{
+					girlsCombinations = girlsCombinations * (girls + 1 - i);
+					divider = divider * i;
+				}
+				girlsCombinations = girlsCombinations / divider;
+				System.out.println("girlsCombinations = " + girlsCombinations);
+				combinations = combinations + boysCombinations * girlsCombinations;
+			}		
+		}
+		System.out.println(combinations);
+	}
+}
