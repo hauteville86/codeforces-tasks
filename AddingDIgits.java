afaf1bcbc0dbfd152a4beebc3c4a8721f6e0009e
Karol Cyprowski <karolcyprowski@gmail.com>
2015-10-13 09:37:40 +0200

    Solve the problem

--- AddingDIgits.java
+++ AddingDIgits.java
@@ -14,7 +14,6 @@ public class AddingDIgits
     {
 	scanner = new Scanner(System.in);
 	getDataFromInput();
-	interval = setInterval();
 	addedDigits = 0;
     }
 
@@ -24,70 +23,50 @@ public class AddingDIgits
 	b = scanner.nextInt();
 	n = scanner.nextInt();
     }
-
-    private static int setInterval()
-    {
-	if(isEven(b)){
-	    return 2;
-	}
-	return 1;
-    }
     
-    private static int findSumOfDigits(int base)
+    private static String findSumOfDigits(int base)
     {
-	int sum = base*10;
-	for(int i = 0; i < 10; i = i + interval){
-	    int nextLevelNumber = sum + i;
-	    if(nextLevelNumber % b == 0){
+	String sumOfDigits = findNumberDividedByb(base);
+        if(sumOfDigits.equals("-1")){
+	    return "-1";	   
+	}
+	if(addedDigits == n){
+	    return sumOfDigits;
+	}
+        return addZerosToNumber(sumOfDigits);	
+    }
+
+    private static String findNumberDividedByb(int sum){
+	for(int i = 0; i < 10; i++){
+	    int sumWithAdditionalDigit = sum*10+i;
+	    if(sumWithAdditionalDigit % b == 0){
 		addedDigits++;
-		if(addedDigits == n){
-		    return nextLevelNumber;
-		}
-		return findSumOfDigits(nextLevelNumber);
+		return Integer.toString(sumWithAdditionalDigit);
 	    }
 	}
-        return -1;
+	return "-1";
+    }
+
+    private static String addZerosToNumber(String number)
+    {
+	String zeros = "0";
+	int zerosNeeded = n-1;
+	for(addedDigits = 1; addedDigits < zerosNeeded/2; addedDigits = addedDigits*2){
+	    zeros = zeros + zeros;
+	}
+        for(addedDigits = addedDigits; addedDigits < zerosNeeded-10; addedDigits = addedDigits + 10){
+	    zeros = zeros + TEN_ZEROS;
+	}
+	for(addedDigits = addedDigits; addedDigits < zerosNeeded; addedDigits++){
+	    zeros = zeros + "0";
+	}
+	return number.concat(zeros);
     }
     
-    private static boolean isEven(int number)
-    {
-	return number % 2 == 0;
-    }
-	
-		
     private static Scanner scanner;
     private static int a;
     private static int b;
     private static int n;
-    private static int interval;
     private static int addedDigits;
+    private final static String TEN_ZEROS = "0000000000";
 }
-// Wrong answer on test 6
-/**
-Time: 77 ms, memory: 1444 KB
-Verdict: WRONG_ANSWER
-Input
-
-100000 10 64479
-
-Participant's output
-
-412542910
-
-Jury's answer
-
-1000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000
-000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000
-00000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000
-00000000000000000000000000000000000000000000000000000000000000...
-Checker comment
-
-wrong answer number has wrong length
-
-
-*/
